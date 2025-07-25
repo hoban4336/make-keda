7c6d39e498fe3181aac2bd0736c7e226db16d78c
hoban4336 <hoban4336@gmail.com>
2025-07-25 18:40:13 +0900

    update alloy

--- makefiles/kong.mk
+++ makefiles/kong.mk
@@ -27,8 +27,9 @@ template_kong: ## Helm Template Kong Install
 
 .PHONY: helm_konga
 install_konga: ## Deploy Konga (admin UI for Kong)
-	@kubectl apply -f kong/konga.yaml --namespace=dev-vsaidt-math && \
-	echo "✅ Konga deployed to namespace kong."
+	namespace=dev-vsaidt-math
+	@kubectl apply -f kong/konga.yaml --namespace=$(namespace) && \
+	echo "✅ Konga deployed to namespace $(namespace)."
 
 .PHONY: uninstall_konga
 uninstall_konga: ## Uninstall Konga only
