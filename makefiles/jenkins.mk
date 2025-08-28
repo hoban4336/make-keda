1f0d5838b8508de13274ca06623197c7ca20eece
hoban4336 <hoban4336@gmail.com>
2025-08-29 00:49:17 +0900

    update

--- makefiles/jenkins.mk
+++ makefiles/jenkins.mk
@@ -7,4 +7,4 @@ deploy_jenkins: ## jenkins 설치
 	helm upgrade --install jenkins jenkinsci/jenkins \
 	-n $(NAMESPACE) \
 	--create-namespace \
-	-f jenkins/value-override.yaml
+	-f jenkins/valus-override.yaml
